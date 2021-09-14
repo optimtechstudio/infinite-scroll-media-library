a06fed135b61dc916c0a386ad4bbee5dc8c9cb61
Malav V <m.vasita@brightness-india.com>
2021-09-14 15:24:28 +0530

    Add: Stable Release Files

--- admin-pages/AdminSettingPage.php
+++ admin-pages/AdminSettingPage.php
@@ -1,8 +1,39 @@
 <?php
 /**
+ * This file is for Admin settings.
+ *
+ * This file responsible for managing the admin settings.
+ * Getting and setting user preferences for functionality.
+ *
+ * PHP version 7.4
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  InfiniteSrollAdmin
+ * @package   OptimPlugin
+ * @author    Optim TechStudio <hello@optimtechstudio.com>
+ * @copyright 2021-2021 Optim TechStudio
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   SVN: <1.0.0>
+ * @link      http://pear.php.net/package/PackageName
+ * @since     File available since Release 1.1.0
+ */
+
+/**
  * This is the class for the Admin Settings.
  *
  * This class initiates the Admin Settings page and allow users to make a choice.
+ *
+ * @category InfiniteSrollAdmin
+ * @package  OptimPlugin
+ * @author   Optim TechStudio <hello@optimtechstudio.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version  Release: <1.0.0>
+ * @link     http://pear.php.net/package/PackageName   
  *
  * @return void.
  */
@@ -15,7 +46,7 @@ Class Admin_Settings_Page
     public function __construct()
     {
         // Creating and Managing Admin Settings Page.
-        $this->optim_admin_settings_page();
+        $this->optimAdminSettingsPage();
     }
 
     /**
@@ -25,10 +56,39 @@ Class Admin_Settings_Page
      *
      * @return void
      */
-    public function optim_admin_settings_page()
+    public function optimAdminSettingsPage()
     {
-        add_action('admin_menu', array( $this, 'optim_add_settings_page' )); // Setting Admin Page
-        add_action('admin_init', array( $this, 'optim_register_settings' )); // Registering Settings
+        add_action( 
+            'admin_menu', 
+            array( $this, 'optimAddSettingPage' ) 
+        ); // Setting Admin Page
+        
+        add_action(
+            'admin_init', 
+            array( $this, 'optimRegisterSettings' )
+        ); // Registering Settings
+
+        add_filter(
+            'plugin_action_links_' . OPTIM_INFINITE_SCROLL_PLUGIN_BASENAME, 
+            array( $this, 'optimPluginSettingsLink' )
+        ); // Add settings link under Plugin list
+
+    }
+
+    /**
+     * Adding Setting link under Plugin List
+     *
+     * @param $links array An array which reflects links
+     * 
+     * @since 1.0.0
+     *
+     * @return array array of links for plugin options.
+     */
+    public function optimPluginSettingsLink($links)
+    {
+        $settings_link = '<a href="options-general.php?page=optim-infinite-scroll">Settings</a>'; // phpcs:ignore
+        array_unshift($links, $settings_link); 
+        return $links;
     }
 
     /**
@@ -38,14 +98,14 @@ Class Admin_Settings_Page
      *
      * @return void
      */
-    public function optim_add_settings_page()
+    public function optimAddSettingPage()
     {
         add_options_page( 
             __('Infinite Scroll in Media Library', 'infinite-scroll-media-library'), 
             __('Media Infinite Scroll', 'infinite-scroll-media-library'), 
             'manage_options', 
             'optim-infinite-scroll', 
-            array( $this, 'optim_is_settings' ) 
+            array( $this, 'optimIsSetting' ) 
         );
     }
 
@@ -56,10 +116,16 @@ Class Admin_Settings_Page
      *
      * @return void
      */
-    public function optim_is_settings()
+    public function optimIsSetting()
     {
         echo '<div class="wrap">
-		<h1>' . __('Enable Infinite Scroll In Media Library', 'infinite-scroll-media-library') . '</h1>
+		<h1>' . 
+            __(
+                'Enable Infinite Scroll In Media Library', 
+                'infinite-scroll-media-library'
+            ) 
+        . '</h1>
+        
 		<form method="post" action="options.php">';
                 
         settings_fields('optim_is_choice'); // settings group name
@@ -71,18 +137,43 @@ Class Admin_Settings_Page
 			<div class="ring-builder-info-div">
 	        <div class="info-left" style="display: inline-block">
 	            <a href="https://optimtechstudio.com/" target="_blank">
-	                <img style="width: 200px;" src="https://optimtechstudio.com/wp-content/uploads/2021/06/logo.png">
+	                <img style="width: 200px;" src="' . 
+                        OPTIM_INFINITE_SCROLL_PLUGIN_DIR . 
+                        "images/optim-logo.png" . '">
 	            </a>
 	        </div>
 
 	        <div class="info-right" style="display: inline-block; margin-left: 2em;">
-	            <h2><b>' . __('Optim Infinite Scroll for Media Library', 'infinite-scroll-media-library') . '</b></h2>
+	            <h2><b>' . __(
+                            'Optim Infinite Scroll for Media Library', 
+                            'infinite-scroll-media-library'
+                        ) 
+                . '</b></h2>
 	            <p>
-	                <b>' . __('Installed Version:', 'infinite-scroll-media-library') . ' v' . OPTIM_INFINITE_SCROLL_MEDIA_LIBRARY . '</b><br>
-	                 ' . __('Website:', 'infinite-scroll-media-library') . ' <a target="_blank" href="https://www.facebook.com/optimtechstudio">Facebook</a>,
-	                <a target="_blank" href="https://in.linkedin.com/in/optim-techstudio-1228b5213">Linked In</a> &amp;
-	                <a target="_blank" href="https://instagram.com/optim_techstudio">Instagram</a>.
-	                <br>' . __('Please contact us on', 'infinite-scroll-media-library') . ' <a href="mailto:infinitescroll@optimtechstudio.com">infinitescroll@optimtechstudio.com</a> '. __('to get in touch.', 'infinite-scroll-media-library') . '
+	                <b>' . __(
+                    'Installed Version:', 'infinite-scroll-media-library'
+                ) . 
+                                ' v' . OPTIM_INFINITE_SCROLL_MEDIA_LIBRARY . 
+                                '</b><br>
+	                 ' . __(
+                                    'Website:', 
+                                    'infinite-scroll-media-library'
+                                ) . 
+                     ' <a target="_blank" href="https://www.facebook.com/optimtechstudio/">Facebook</a>,
+	                <a target="_blank" href="https://www.linkedin.com/in/optim-techstudio-1228b5213/">LinkedIn</a> &amp;
+	                <a target="_blank" href="https://instagram.com/optim_techstudio/">Instagram</a>.
+	                <br>' . 
+                        __(
+                            'Please contact us on', 
+                            'infinite-scroll-media-library'
+                        ) . 
+                    ' <a href="mailto:infinitescroll@optimtechstudio.com">
+                        infinitescroll@optimtechstudio.com
+                    </a> '. 
+                        __(
+                            'to get in touch.', 
+                            'infinite-scroll-media-library'
+                        ) . '
 	            </p>
 	        </div>
 	        <hr/>
@@ -96,7 +187,7 @@ Class Admin_Settings_Page
      *
      * @return void
      */
-    public function optim_register_settings()
+    public function optimRegisterSettings()
     {
 
         register_setting(
@@ -115,7 +206,10 @@ Class Admin_Settings_Page
         add_settings_field(
             'optim_is_choice',
             __('Toggle for Infinite Scroll', 'infinite-scroll-media-library'),
-            array( $this, 'optim_infinite_scroll_settings_section' ), // function which prints the field
+            array( 
+                $this, 
+                'optimInfiniteScrollSettingsSelection' 
+            ), // function which prints the field
             'optim-infinite-scroll', // page slug
             'optim_is_option_section', // section ID
             array( 
@@ -133,12 +227,25 @@ Class Admin_Settings_Page
      *
      * @return void
      */
-    public function optim_infinite_scroll_settings_section()
+    public function optimInfiniteScrollSettingsSelection()
     {
         $optim_is_choice = get_option('optim_is_choice');
 
-        echo '<input type="radio" ' . ( '1' == $optim_is_choice ? 'checked' : '' ) . ' name="optim_is_choice" value="1" /> ' . __('Enable', 'infinite-scroll-media-library')
-        . '<input type="radio" ' . ( '0' == $optim_is_choice ? 'checked' : '' ) . ' name="optim_is_choice" value="0" /> ' . __('Disable', 'infinite-scroll-media-library');
+        echo '<input type="radio" ' . 
+            ( 
+                '1' == $optim_is_choice ? 'checked' : '' 
+            ) . ' name="optim_is_choice" value="1" /> ' . 
+            __(
+                'Enable ', 
+                'infinite-scroll-media-library'
+            )
+        . ' <input type="radio" ' . 
+        ( '0' == $optim_is_choice ? 'checked' : '' ) . 
+        ' name="optim_is_choice" value="0" /> ' . 
+        __(
+            'Disable', 
+            'infinite-scroll-media-library'
+        );
     }
 
 }
--- infinite-scrolling-media-library.php
+++ infinite-scrolling-media-library.php
@@ -1,13 +1,39 @@
 <?php
 /**
+ * This file initiates the functionality.
+ *
+ * This file responsible for exectuting main purpose for the plugin.
+ * From Admin Settings to WordPress Media Library, all thigs are
+ * Covered in this file. 
+ *
+ * PHP version 7.4
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  InfiniteSroll
+ * @package   OptimPlugin
+ * @author    Optim TechStudio <hello@optimtechstudio.com>
+ * @copyright 2021-2021 Optim TechStudio
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   SVN: <1.0.0>
+ * @link      http://pear.php.net/package/PackageName
+ * @since     File available since Release 1.1.0
+ */
+
+/**
  * Plugin Name:       Infinite Scroll in Media Library
- * Plugin URI:        https://optimtechstudio.com/
- * Description:       Get back the Infinite Scrolling in Media Gallery with This Plugin. WordPress has removed the infinite scrolling since v5.8.0.
+ * Plugin URI:        https://wordpress.org/plugins/infinite-scroll-in-media-library/
+ * Description:       Get back the Infinite Scrolling in Media Gallery 
+ * with This Plugin. WordPress has removed the infinite scrolling since v5.8.0.
  * Version:           1.0.0
  * Requires at least: 5.8
  * Requires PHP:      7.2
  * Author:            Optim TechStudio
- * Author URI:        https://malavvasita.github.io/
+ * Author URI:        https://optimtechstudio.com
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       infinite-scroll-media-library
@@ -15,24 +41,45 @@
  */
 
 // Don't allow if user access this file directly
- defined( 'ABSPATH' ) || die( __( 'You are not allowed to access this file directly.', 'infinite-scroll-media-library' ) );
+defined('ABSPATH') || 
+die(
+    __(
+        'You are not allowed to access this file directly.', 
+        'infinite-scroll-media-library'
+    )
+);
 
 // Version Number for Future Release Maintenance
- define( 'OPTIM_INFINITE_SCROLL_MEDIA_LIBRARY', '1.0.0' );
+define('OPTIM_INFINITE_SCROLL_MEDIA_LIBRARY', '1.0.0');
+
+// Plugin directory URL for global use
+define('OPTIM_INFINITE_SCROLL_PLUGIN_DIR', plugin_dir_url(__FILE__));
+
+// Plugin base name
+define('OPTIM_INFINITE_SCROLL_PLUGIN_BASENAME', plugin_basename(__FILE__));
 
 /**
  * This is the main class of the plugin.
  *
  * This class initiates the proceedings of Making Infinite scroll functionality.
+ * 
+ * @category InfiniteSroll
+ * @package  OptimPlugin
+ * @author   Optim TechStudio <hello@optimtechstudio.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version  Release: <1.0.0>
+ * @link     http://pear.php.net/package/PackageName   
  *
  * @return void.
  */
-class Infinite_Scroll_In_Media_Library {
+class InfiniteScrollInMediaLibrary
+{
     /**
      *  Constructor.
      */
-    public function __construct() {
-        $this->optim_infinite_scroll_hooks();
+    public function __construct()
+    {
+        $this->optimInfiniteScrollHooks();
     }
 
     /**
@@ -42,25 +89,26 @@ class Infinite_Scroll_In_Media_Library {
      *
      * @return void
      */
-    public function optim_infinite_scroll_hooks() {
+    public function optimInfiniteScrollHooks()
+    {
         
         // If the option is not set for Infinite Scroll, Enable it by default.
-        if ( ! get_option( "optim_is_choice" ) ) {
-             add_option( "optim_is_choice", "1" );
+        if (! get_option("optim_is_choice") ) {
+             add_option("optim_is_choice", "1");
         }
 
-        $optim_is_user_choice = get_option( 'optim_is_choice' );
+        $optim_is_user_choice = get_option('optim_is_choice');
 
-        if( !empty( $optim_is_user_choice ) && 0 !== $optim_is_user_choice ){
+        if (!empty($optim_is_user_choice) && 0 !== $optim_is_user_choice ) {
             // Filter to Enable the Infinite Scroll in Media Library
-            add_filter( 'media_library_infinite_scrolling', '__return_true' );
+            add_filter('media_library_infinite_scrolling', '__return_true');
         }
 
     }
 }
 
-new Infinite_Scroll_In_Media_Library(); // Initialize of the Main Class
+new InfiniteScrollInMediaLibrary(); // Initialize of the Main Class
 
 // Page for the Admin Settings for User's Choice
-require_once( dirname(__FILE__) . '/admin-pages/AdminSettingPage.php' );
+require_once dirname(__FILE__) . '/admin-pages/AdminSettingPage.php';
 new Admin_Settings_Page(); // Initialization of Admin Settings Page
